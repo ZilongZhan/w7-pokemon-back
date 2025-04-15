1a624cdbbf097fde89ef6071e5d15936dbf2dbae
Zilong Zhan <zolong.zhan@gmail.com>
2025-04-15 05:02:32 +0200

    Add pokemon fixture data

---
+++ src/pokemon/fixtures.ts
@@ -0,0 +1,7 @@
+import Pokemon from "./Pokemon.js";
+
+const plusle = new Pokemon(311, "Plusle", "http://plusle.com/plusle.webp");
+
+const minun = new Pokemon(312, "Minun", "http://minun.com/minun.webp");
+
+export { plusle, minun };
