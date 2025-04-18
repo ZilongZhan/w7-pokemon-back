f76119f6beb5e15c0718d3eec2fb38e325b80d18
Zilong Zhan <zolong.zhan@gmail.com>
2025-04-18 22:58:30 +0200

    Add removePokemonById method to PokemonController

--- src/pokemon/controller/types.ts
+++ src/pokemon/controller/types.ts
@@ -2,4 +2,6 @@ import { Request, Response } from "express";
 
 export interface PokemonControllerStructure {
   getAllPokemons: (_req: Request, res: Response) => void;
+  addPokemon: (req: Request, res: Response) => void;
+  removePokemonById: (req: Request, res: Response) => void;
 }
--- src/pokemon/fixtures.ts
+++ src/pokemon/fixtures.ts
@@ -1,17 +1,23 @@
 import Pokemon from "./Pokemon.js";
 
-const plusle = new Pokemon({
-  pokeIndex: 311,
-  name: "Plusle",
-  imageUrl: "http://plusle.com/plusle.webp",
-  isCaptured: false,
-});
+const plusle = new Pokemon(
+  {
+    pokeIndex: 311,
+    name: "Plusle",
+    imageUrl: "http://plusle.com/plusle.webp",
+    isCaptured: false,
+  },
+  () => "plusleId",
+);
 
-const minun = new Pokemon({
-  pokeIndex: 312,
-  name: "Minun",
-  imageUrl: "http://minun.com/minun.webp",
-  isCaptured: true,
-});
+const minun = new Pokemon(
+  {
+    pokeIndex: 312,
+    name: "Minun",
+    imageUrl: "http://minun.com/minun.webp",
+    isCaptured: true,
+  },
+  () => "minunId",
+);
 
 export { plusle, minun };
