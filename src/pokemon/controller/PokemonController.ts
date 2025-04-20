e1ba82b83c6350088a3ed39cc4f40765eea5d3f7
Zilong Zhan <zolong.zhan@gmail.com>
2025-04-20 23:21:42 +0200

    Add capturePokemonById method to PokemonController

--- src/pokemon/controller/PokemonController.ts
+++ src/pokemon/controller/PokemonController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import Pokemon from "../Pokemon.js";
 import { PokemonControllerStructure } from "./types.js";
-import { PokemonData } from "../types.js";
+import { PokemonData, PokemonStructure } from "../types.js";
 
 class PokemonController implements PokemonControllerStructure {
   constructor(private pokemons: Pokemon[]) {}
@@ -13,9 +13,7 @@ class PokemonController implements PokemonControllerStructure {
   public addPokemon = (req: Request, res: Response): void => {
     const pokemonData = req.body as PokemonData;
 
-    const pokemonExists = this.pokemons.some(
-      (pokemon) => pokemon.name === pokemonData.name,
-    );
+    const pokemonExists = this.doesPokemonExist(pokemonData.name);
 
     if (pokemonExists) {
       res.status(409).json({ error: "Pokemon already exists" });
@@ -32,9 +30,7 @@ class PokemonController implements PokemonControllerStructure {
   public removePokemonById = (req: Request, res: Response): void => {
     const { pokemonId } = req.params;
 
-    const pokemonExists = this.pokemons.some(
-      (pokemon) => pokemon.id === pokemonId,
-    );
+    const pokemonExists = this.doesPokemonExist(pokemonId);
 
     if (!pokemonExists) {
       res.status(404).json({ error: "Pokemon doesn't exist" });
@@ -50,6 +46,35 @@ class PokemonController implements PokemonControllerStructure {
 
     res.status(200).json(pokemon);
   };
+
+  public capturePokemonById(req: Request, res: Response): void {
+    const { pokemonId } = req.params;
+    const { isCaptured } = req.body as Pick<PokemonStructure, "isCaptured">;
+
+    const pokemonExists = this.doesPokemonExist(pokemonId);
+
+    if (!pokemonExists) {
+      res.status(404).json({ error: "Pokemon doesn't exist" });
+      return;
+    }
+
+    const pokemon = this.pokemons.find((pokemon) => pokemon.id === pokemonId)!;
+
+    if (pokemon.isCaptured) {
+      res.status(409).json({ error: "Pokemon is already captured" });
+      return;
+    }
+
+    pokemon.isCaptured = isCaptured;
+
+    res.status(200).json(pokemon);
+  }
+
+  private doesPokemonExist(searchValue: string): boolean {
+    return this.pokemons.some(
+      (pokemon) => pokemon.name === searchValue || pokemon.id === searchValue,
+    );
+  }
 }
 
 export default PokemonController;
--- src/pokemon/controller/types.ts
+++ src/pokemon/controller/types.ts
@@ -4,4 +4,5 @@ export interface PokemonControllerStructure {
   getAllPokemons: (_req: Request, res: Response) => void;
   addPokemon: (req: Request, res: Response) => void;
   removePokemonById: (req: Request, res: Response) => void;
+  capturePokemonById: (req: Request, res: Response) => void;
 }
